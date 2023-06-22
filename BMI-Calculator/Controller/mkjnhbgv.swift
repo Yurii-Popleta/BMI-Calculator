e9eb00c05aab7e2168daa3e34907bae31c93166d
Yurii Popleta <yurka279@gmail.com>
2023-06-22 12:29:40 +0200

    UpdateUI

---
+++ BMI-Calculator/Controller/mkjnhbgv.swift
@@ -0,0 +1,9 @@
+//
+//  mkjnhbgv.swift
+//  Magic 8 Ball
+//
+//  Created by Admin on 05/10/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ BMI-Calculator/View/NestedStackView.swift
@@ -0,0 +1,32 @@
+//
+//  NestedStackView.swift
+//  BMI-Calculator
+//
+//  Created by Admin on 22/06/2023.
+//
+
+import UIKit
+
+class NestedStackView: UIStackView {
+    let leftLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .light)
+        label.textColor = .darkGray
+        return label
+    }()
+    let rightLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .light)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        addArrangedSubview(leftLabel)
+        addArrangedSubview(rightLabel)
+    }
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
